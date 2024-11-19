641871acf294da05235e64e7d022f1f0c715a2d7
yehiasalah <yehiasalah74@gmail.com>
2024-11-19 13:42:17 +0200

    update models

--- accounts/models.py
+++ accounts/models.py
@@ -82,3 +82,32 @@ class User(AbstractBaseUser):
         elif self.role == 2:
             user_role = 'Customer'
         return user_role
+
+class UserProfile(models.Model):
+    user  = models.OneToOneField(User,on_delete=models.CASCADE, blank=True , null=True)
+    profile_pricuture = models.ImageField(upload_to='users/profile_pictures',blank= True ,null= True)
+    Cover_photo = models.ImageField(upload_to='users/cover_photos',blank= True ,null= True)
+    addrees_line_1 = models.CharField (max_length= 50 ,blank= True ,null= True)
+    addrees_line_2 = models.CharField (max_length= 50 ,blank= True ,null= True)
+    country = models.CharField (max_length= 15 ,blank= True ,null= True)
+    state = models.CharField (max_length= 15 ,blank= True ,null= True)
+    city = models.CharField (max_length= 15 ,blank= True ,null= True)
+    pin_code = models.CharField (max_length= 6 ,blank= True ,null= True)
+    latitude = models.CharField (max_length= 20 ,blank= True ,null= True)
+    longitude = models.CharField (max_length= 20 ,blank= True ,null= True)
+    Created_at = models.DateTimeField(auto_now= True)
+    modified_at = models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+         return self.user.email
+    
+
+
+
+
+
+
+ 
+
+
+
